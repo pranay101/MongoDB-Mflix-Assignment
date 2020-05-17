250cc54187a1da46debb1c4900a48d78e1076e80
pranay101 <pranayprajapati101@gmail.com>
2020-05-17 13:35:27 +0530

    Implemented Faceted search

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -196,6 +196,10 @@ export default class MoviesDAO {
     const queryPipeline = [
       matchStage,
       sortStage,
+      skipStage,
+      limitStage,
+      facetStage,
+
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
     ]
